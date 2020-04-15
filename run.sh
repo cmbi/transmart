bb030e426153e1d95bcf744cf1d3be842f6bc2a7
Coos Baakman <c.baakman@radboudumc.nl>
2020-04-15 08:32:09 +0200

    start solr as well

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+source /root/transmart/transmart-data/vars
 
 export CATALINA_HOME=/usr/share/tomcat7
 export CATALINA_BASE=/var/lib/tomcat7
 
+nohup make -C /root/transmart/transmart-data/solr start > ~/transmart/transmart-data/solr.log 2>&1 &
 /usr/local/bin/R CMD Rserve --vanilla
 service postgresql start
 /usr/share/tomcat7/bin/catalina.sh run
